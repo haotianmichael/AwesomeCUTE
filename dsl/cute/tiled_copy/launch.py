934a6501556a50a483d664368dbfd439c3f1ac23
haotianmichael <codesawmichael@gmail.com>
2026-04-03 15:07:24 +0800

    fix(torch): fix bug of mislocation of dir

--- dsl/cute/tiled_copy/launch.py
+++ dsl/cute/tiled_copy/launch.py
@@ -5,7 +5,7 @@ from torch.utils.cpp_extension import load
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-cutlass_include_path = os.path.join(current_dir, "../../third-party/cutlass/include")
+cutlass_include_path = os.path.join(current_dir, "../../../third-party/cutlass/include")
 sources = [os.path.join(current_dir, filename) for filename in ["tiled_copy.cuh"]]
 
 os.environ["TORCH_CUDA_ARCH_LIST"] = ".".join(map(str, torch.cuda.get_device_capability()))
