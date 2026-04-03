794e839ddec4e4972bc4b1e6cf5acfcea9fec260
haotianmichael <codesawmichael@gmail.com>
2026-04-03 16:40:10 +0800

    fix(ptx): fix bug of ptx asm

--- dsl/cute/mixed_presicion_hgemm/mixed_precision_gemm.cuh
+++ dsl/cute/mixed_presicion_hgemm/mixed_precision_gemm.cuh
@@ -22,15 +22,16 @@ namespace cute {
 struct SM90_16x8x32_F32E4M3E5M2F32_TN{
     using DRegisters = float[4];
     using ARegisters = uint32_t[4];
-    using BRegsiters = uint32_t[2];
-    using CRegsiters = float[4];
+    using BRegisters = uint32_t[2];
+    using CRegisters = float[4];
 
-    CUTE_HOST_DEVICE static void fma(float &d0,
+  CUTE_HOST_DEVICE static void fma(float &d0,
                                      float &d1,
                                      float &d2, 
                                      float &d3,
                                      uint32_t const &a0,
                                      uint32_t const &a1,
+                                     uint32_t const &a2,
                                      uint32_t const &a3,
                                      uint32_t const &b0,
                                      uint32_t const &b1,
@@ -39,10 +40,10 @@ struct SM90_16x8x32_F32E4M3E5M2F32_TN{
                                      float const &c2,
                                      float const &c3) {
 #if defined(CUTE_ARCH_MMA_SM89_ENABLED)
-    asm volatile("mma.sync.aligned.m16n8k32.row.col.e4m3.e5m2.f32 "
+    asm volatile("mma.sync.aligned.m16n8k32.row.col.f32.e4m3.e5m2.f32 "
                 "{%0, %1, %2, %3},"
-                "{%4, %5, %6, %7},
-                "{%8, %9},
+                "{%4, %5, %6, %7},"
+                "{%8, %9},"
                 "{%10, %11, %12, %13};\n"
                 : "=f"(d0), "=f"(d1), "=f"(d2), "=f"(d3)
                 : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(b0), "r"(b1), "f"(c0), "f"(c1), "f"(c2), "f"(c3));
